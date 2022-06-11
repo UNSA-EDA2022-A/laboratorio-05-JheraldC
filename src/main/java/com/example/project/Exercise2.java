fe3f227090be38fbbbe0da5115cb5cfadcfa5b7a
JHERALD HUREN CACERES APAZA <93649530+JheraldC@users.noreply.github.com>
2022-06-11 18:10:50 -0500

    Update Exercise2.java

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -12,10 +12,24 @@ public class Exercise2 {
         }
     }
 
-    public boolean existenDuplicados(String str) {
-        MyStack<Character> stack = new LinkedListStack<>();
-        // Colocar codigo aqui
-
-        return false;
-    }
+	public boolean existenDuplicados(String str) {
+		MyStack<Character> stack = new LinkedListStack<>();
+		// Colocar codigo aqui
+		for (int i = 0; i < str.length(); i++) { //Bucle para recorrer los caracteres del String
+			char ch = str.charAt(i); //Guardo el caracter en una variable tipo char
+			if (ch == ')') { //Verifico si se cierra el corchete
+				stack.pop(); //Si es cerrado, elimino el ultimo elemento agregado
+				int contElem = 0; //Inicio un contador de los elementos
+				while (stack.top() != '(') { //Bucle para recorrer los caracteres hasta encontrar el corchete abierto
+					contElem++; //Si no lo encuentra, suma al contador
+					stack.pop(); //Elimina de la pila el ultimo elemento agregado, encontro su corchete 
+				}
+				if (contElem < 1) { //Si el contador es menor a 1, significa que encontro un corchete duplicado
+					return true;  //Retorna verdadero
+				}
+			} else //Si no cierra, es abierto
+				stack.push(ch); //Entonces se agrega a la pila
+		}
+		return false; //Si no es menor a 1, entonces no se encontraron corchetes duplicados
+	}
 }
